fcd8ecc268edded4d99d9e1ddb999ff8b8cc8245
Kyle Xiong <kylexiong.bear@gmail.com>
2018-08-01 10:24:25 -0400

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Kyle on 7/31/18.
+//  Copyright © 2018 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var gameTitle: UILabel!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        //sender is the object that calls this method (button)
+        let title = sender.title(for: .normal)!
+        gameTitle.text = "\(title)"
+    }
+    
+    @IBOutlet weak var gatlinGunImage: UIImageView!
+    @IBOutlet weak var sniperImage: UIImageView!
+    @IBOutlet weak var gatlinGunHealthDisplay: UILabel!
+    @IBOutlet weak var sniperHealthDisplay: UILabel!
+    var gatlinGunHealth = 200
+    var sniperHealth = 25
+    
+    func refresh() {
+        if (gatlinGunHealth <= 0) {
+            gatlinGunHealthDisplay.text = "Health: 0"
+            gatlinGunImage.image = UIImage(named: "explosion.jpg")
+        }else{
+            gatlinGunHealthDisplay.text = "Health: \(gatlinGunHealth) "
+        }
+        if (sniperHealth <= 0){
+            sniperHealthDisplay.text = "Health: 0"
+            sniperImage.image = UIImage(named: "explosion.jpg")
+        }else{
+            sniperHealthDisplay.text = "Health: \(sniperHealth) "
+        }
+    }
+    @IBAction func gatlinGunAttack(_ sender: UIButton) {
+        sniperHealth -= 2
+        refresh()
+    }
+    @IBAction func sniperAttack(_ sender: UIButton) {
+        gatlinGunHealth -= 25
+        refresh()
+    }
+    
+    
+    
+    
+}
